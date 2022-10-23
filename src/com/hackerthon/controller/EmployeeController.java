0254afd163ea2b2472384fd40aed9d32ad874ce7
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-10-23 19:38:39 +0530

    employee controller

--- src/com/hackerthon/controller/EmployeeController.java
+++ src/com/hackerthon/controller/EmployeeController.java
@@ -1,24 +1,34 @@
 package com.hackerthon.controller;
 
+import com.hackerthon.bo.BOFactory;
+import com.hackerthon.bo.custom.EmployeeBO;
+import com.hackerthon.entity.Employee;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class EmployeeController {
+
+	static EmployeeBO bo = (EmployeeBO) BOFactory.getInstance().getBOType(BOFactory.BOType.EMPLOYEE);
+
 	public boolean addEmployee(Employee employee) throws SQLException, Exception {
-		return false;
+		return bo.addEmployee(employee);
 	}
 
 	public boolean deleteEmployee(String id) throws SQLException, Exception {
-		return false;
+		return bo.deleteEmployee(id);
 	}
 
 	public boolean updateEmployee(Employee employee) throws SQLException, Exception {
-		return false;
+		return bo.updateEmployee(employee);
 	}
 
 	public Employee searchEmployee(String id) throws SQLException, Exception {
-		return null;
+		return bo.searchEmployee(id);
 	}
 
 	public ArrayList<Employee> getAllEmployee() throws SQLException, Exception {
-		return null;
+		return bo.getAllEmployee();
 	}
 
 }
