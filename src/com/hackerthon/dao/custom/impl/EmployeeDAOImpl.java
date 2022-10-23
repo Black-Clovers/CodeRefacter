7bf4df1b4423595c77350f1cb81250afbbf477b0
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-10-23 23:51:03 +0530

    daoImpl is done

--- src/com/hackerthon/dao/custom/impl/EmployeeDAOImpl.java
+++ src/com/hackerthon/dao/custom/impl/EmployeeDAOImpl.java
@@ -1,34 +1,17 @@
 package com.hackerthon.dao.custom.impl;
 
-import com.hackerthon.dao.custom.EmloyeeDAO;
+import com.hackerthon.dao.CrudUtil;
+import com.hackerthon.dao.custom.EmployeeDAO;
 import com.hackerthon.entity.Employee;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EmployeeDAOImpl implements EmloyeeDAO {
-	@Override public boolean save(Employee employee) throws Exception {
-		return false;
-	}
+public class EmployeeDAOImpl implements EmployeeDAO {
 
-	@Override public boolean update(Employee employee) throws Exception {
-		return false;
-	}
-
-	@Override public boolean delete(String s) throws Exception {
-		return false;
-	}
-
-	@Override public Employee getByID(String s) throws Exception {
-		return null;
-	}
-
-	@Override public List<Employee> getAll() throws Exception {
-
-		return null;
-	}
-
-	@Override public void getEmployeeOutPut(ArrayList<Employee> employeeList) throws Exception {
+	@Override public void getEmployeeOutPut() throws Exception {
+		ArrayList<Employee> employeeList = (ArrayList<Employee>) getAll();
 		System.out.println("Employee ID" + "\t\t" + "Full Name" + "\t\t" + "Address" + "\t\t" + "Faculty Name" + "\t\t"
 				+ "Department" + "\t\t" + "Designation" + "\n");
 		System.out.println(
@@ -43,4 +26,57 @@ public class EmployeeDAOImpl implements EmloyeeDAO {
 					"----------------------------------------------------------------------------------------------------------------");
 		}
 	}
+
+	@Override public void createTable() throws Exception {
+		CrudUtil.executeUpdate(CrudUtil.getEmployeeQueryById("q1"));
+		CrudUtil.executeUpdate(CrudUtil.getEmployeeQueryById("q2"));
+	}
+
+	@Override public boolean save(Employee employee) throws Exception {
+		return CrudUtil.executeUpdate(CrudUtil.getEmployeeQueryById("q3"), employee.getEmployeeID(),
+				employee.getFullName(), employee.getAddress(), employee.getFacultyName(), employee.getDepartment(),
+				employee.getDesignation());
+	}
+
+	@Override public boolean update(Employee employee) throws Exception {
+		return CrudUtil.executeUpdate(CrudUtil.getEmployeeQueryById("q4"), employee.getFullName(),
+				employee.getAddress(), employee.getFacultyName(), employee.getDepartment(), employee.getDesignation(),
+				employee.getEmployeeID());
+	}
+
+	@Override public boolean delete(String id) throws Exception {
+		return CrudUtil.executeUpdate(CrudUtil.getEmployeeQueryById("q6"), id);
+	}
+
+	@Override public Employee getByID(String id) throws Exception {
+		ResultSet rst = CrudUtil.executeQuery(CrudUtil.getEmployeeQueryById("q5"), id);
+		Employee employee = null;
+		while (rst.next()) {
+			employee = new Employee();
+			employee.setEmployeeID(rst.getString(1));
+			employee.setFullName(rst.getString(2));
+			employee.setAddress(rst.getString(3));
+			employee.setFacultyName(rst.getString(4));
+			employee.setDepartment(rst.getString(5));
+			employee.setDesignation(rst.getString(6));
+		}
+		return employee;
+	}
+
+	@Override public List<Employee> getAll() throws Exception {
+		ResultSet rst = CrudUtil.executeQuery("SELECT * FROM customer");
+		ArrayList<Employee> allEmployees = new ArrayList<>();
+		while (rst.next()) {
+			Employee employee = new Employee();
+			employee.setEmployeeID(rst.getString(1));
+			employee.setFullName(rst.getString(2));
+			employee.setAddress(rst.getString(3));
+			employee.setFacultyName(rst.getString(4));
+			employee.setDepartment(rst.getString(5));
+			employee.setDesignation(rst.getString(6));
+			allEmployees.add(employee);
+		}
+		return allEmployees;
+	}
+
 }
