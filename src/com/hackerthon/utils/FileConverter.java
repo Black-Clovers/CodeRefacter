d1e8d00dd93b1888e2ab1a76ecb7598f5dc5a107
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-10-24 09:41:16 +0530

    add comments to the file converter file

--- src/com/hackerthon/utils/FileConverter.java
+++ src/com/hackerthon/utils/FileConverter.java
@@ -18,43 +18,56 @@ import java.util.Map;
 
 public class FileConverter {
 
-	private static final ArrayList<Map<String, String>> l = new ArrayList<Map<String, String>>();
+	//create arraylist to store the employee details
+	private static final ArrayList<Map<String, String>> employeeList = new ArrayList<Map<String, String>>();
 
-	private static Map<String, String> m = null;
+	//create map to store the xpaths
+	private static Map<String, String> map = null;
 
-	public static void getRequestTransform() throws Exception {
+	public static void getRequestTransform()
+			throws Exception { //create method to transform the xml file and throw exception
 		// get EmployeeRequest.xml file
 		Source employeeRequest = new StreamSource(new File("src/com/hackerthon/utils/EmployeeRequest.xml"));
 		// get Employee-modified.xsl file
 		Source employeeModified = new StreamSource(new File("src/com/hackerthon/utils/Employee-modified.xsl"));
 		// get EmployeeResponse.xml file
 		Result employeeResponse = new StreamResult(new File("src/com/hackerthon/utils/EmployeeResponse.xml"));
+		// create TransformerFactory instance and transform
 		TransformerFactory.newInstance().newTransformer(employeeModified).transform(employeeRequest, employeeResponse);
 	}
 
-	public static ArrayList<Map<String, String>> getXmlXPaths() throws Exception {
+	public static ArrayList<Map<String, String>> getXmlXPaths()
+			throws Exception { //create method to get the xpaths and throw exception
 
+		//create DocumentBuilderFactory instance and parse the xml file
 		Document d = DocumentBuilderFactory.newInstance().newDocumentBuilder()
 				.parse("src/com/hackerthon/utils/EmployeeResponse.xml");
-		XPath x = XPathFactory.newInstance().newXPath();
-		int n = Integer.parseInt((String) x.compile("count(//Employees/Employee)").evaluate(d, XPathConstants.STRING));
-		for (int i = 1; i <= n; i++) {
-			m = new HashMap<String, String>();
-			m.put("XpathEmployeeIDKey", (String) x.compile("//Employees/Employee[" + i + "]/EmployeeID/text()")
-					.evaluate(d, XPathConstants.STRING));
-			m.put("XpathEmployeeNameKey", (String) x.compile("//Employees/Employee[" + i + "]/EmployeeFullName/text()")
-					.evaluate(d, XPathConstants.STRING));
-			m.put("XpathEmployeeAddressKey",
-					(String) x.compile("//Employees/Employee[" + i + "]/EmployeeFullAddress/text()")
-							.evaluate(d, XPathConstants.STRING));
-			m.put("XpathFacultyNameKey", (String) x.compile("//Employees/Employee[" + i + "]/FacultyName/text()")
-					.evaluate(d, XPathConstants.STRING));
-			m.put("XpathDepartmentKey", (String) x.compile("//Employees/Employee[" + i + "]/Department/text()")
-					.evaluate(d, XPathConstants.STRING));
-			m.put("XpathDesignationKey", (String) x.compile("//Employees/Employee[" + i + "]/Designation/text()")
-					.evaluate(d, XPathConstants.STRING));
-			l.add(m);
+
+		//create XPathFactory instance and create new XPath
+		XPath xPath = XPathFactory.newInstance().newXPath();
+
+		//pass count of string value to int
+		int count = Integer.parseInt(
+				(String) xPath.compile("count(//Employees/Employee)").evaluate(d, XPathConstants.STRING));
+
+		for (int i = 1; i <= count; i++) {//iterate the count
+			map = new HashMap<String, String>();//create new map
+			map.put("XpathEmployeeIDKey", (String) xPath.compile("//Employees/Employee[" + i + "]/EmployeeID/text()")
+					.evaluate(d, XPathConstants.STRING));//get the xpath of employee id
+			map.put("XpathEmployeeNameKey",
+					(String) xPath.compile("//Employees/Employee[" + i + "]/EmployeeFullName/text()")
+							.evaluate(d, XPathConstants.STRING));//get the xpath of employee name
+			map.put("XpathEmployeeAddressKey",
+					(String) xPath.compile("//Employees/Employee[" + i + "]/EmployeeFullAddress/text()")
+							.evaluate(d, XPathConstants.STRING));//get the xpath of employee address
+			map.put("XpathFacultyNameKey", (String) xPath.compile("//Employees/Employee[" + i + "]/FacultyName/text()")
+					.evaluate(d, XPathConstants.STRING));//get the xpath of faculty name
+			map.put("XpathDepartmentKey", (String) xPath.compile("//Employees/Employee[" + i + "]/Department/text()")
+					.evaluate(d, XPathConstants.STRING));//get the xpath of department
+			map.put("XpathDesignationKey", (String) xPath.compile("//Employees/Employee[" + i + "]/Designation/text()")
+					.evaluate(d, XPathConstants.STRING));//get the xpath of designation
+			employeeList.add(map);//add the map to the arraylist
 		}
-		return l;
+		return employeeList;//return the arraylist
 	}
 }
