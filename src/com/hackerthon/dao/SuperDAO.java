07b09713dcf677f2b55550fdb170bbd4554da512
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-10-24 10:21:23 +0530

    add comments to the SuperDAO class

--- src/com/hackerthon/dao/SuperDAO.java
+++ src/com/hackerthon/dao/SuperDAO.java
@@ -1,5 +1,5 @@
 package com.hackerthon.dao;
 
-public interface SuperDAO {
-	void createTable() throws Exception;
+public interface SuperDAO { // This is the interface class for all the DAO classes to implement methods
+	void createTable() throws Exception; // This method is used to create the table in the database
 }
