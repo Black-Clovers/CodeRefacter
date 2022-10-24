f8f20a95d2078016558f4091cab6b13c111bb0b5
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-10-24 10:19:02 +0530

    add comments to the Employee class

--- src/com/hackerthon/entity/Employee.java
+++ src/com/hackerthon/entity/Employee.java
@@ -1,64 +1,74 @@
 package com.hackerthon.entity;
 
-public class Employee {
-	private String employeeID;
-	private String fullName;
-	private String address;
-	private String facultyName;
-	private String department;
-	private String designation;
+public class Employee {//Employee class
+	private String employeeID;// create a variable to store employeeID
+	private String fullName;// create a variable to store fullName
+	private String address;// create a variable to store address
+	private String facultyName;// create a variable to store facultyName
+	private String department;// create a variable to store department
+	private String designation;// create a variable to store designation
 
-	public String getEmployeeID() {
-		return employeeID;
+	public String getEmployeeID() {// create a method to get employeeID
+
+		return employeeID;// return employeeID
 	}
 
-	public void setEmployeeID(String employeeID) {
-		this.employeeID = employeeID;
+	public void setEmployeeID(String employeeID) {// create a method to set employeeID
+
+		this.employeeID = employeeID;// set employeeID
 	}
 
-	public String getFullName() {
-		return fullName;
+	public String getFullName() {// create a method to get fullName
+		return fullName;// return fullName
 	}
 
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
+	public void setFullName(String fullName) {// create a method to set fullName
+		this.fullName = fullName;// set fullName
 	}
 
-	public String getAddress() {
-		return address;
+	public String getAddress() {// create a method to get address
+
+		return address;// return address
 	}
 
-	public void setAddress(String address) {
-		this.address = address;
+	public void setAddress(String address) {// create a method to set address
+
+		this.address = address;// set address
 	}
 
-	public String getFacultyName() {
-		return facultyName;
+	public String getFacultyName() {// create a method to get facultyName
+
+		return facultyName;// return facultyName
 	}
 
-	public void setFacultyName(String facultyName) {
-		this.facultyName = facultyName;
+	public void setFacultyName(String facultyName) {// create a method to set facultyName
+
+		this.facultyName = facultyName;// set facultyName
 	}
 
-	public String getDepartment() {
-		return department;
+	public String getDepartment() {// create a method to get department
+
+		return department;// return department
 	}
 
-	public void setDepartment(String department) {
-		this.department = department;
+	public void setDepartment(String department) {// create a method to set department
+
+		this.department = department;// set department
 	}
 
-	public String getDesignation() {
-		return designation;
+	public String getDesignation() {// create a method to get designation
+
+		return designation;// return designation
 	}
 
-	public void setDesignation(String designation) {
-		this.designation = designation;
+	public void setDesignation(String designation) {// create a method to set designation
+
+		this.designation = designation;// set designation
 	}
 
-	@Override public String toString() {
+	@Override public String toString() {// create a method to print the details of employee
 		return "Employee{" + "employeeID='" + employeeID + '\'' + ", fullName='" + fullName + '\'' + ", address='"
 				+ address + '\'' + ", facultyName='" + facultyName + '\'' + ", department='" + department + '\''
-				+ ", designation='" + designation + '\'' + '}';
+				+ ", designation='" + designation + '\'' + '}';// return the details of employee
 	}
 }
