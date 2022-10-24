8ad9450b8810f2de639f6bd42bb359c5677d919d
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-10-24 09:56:58 +0530

    add comments to the EmployeeController class

--- src/com/hackerthon/controller/EmployeeController.java
+++ src/com/hackerthon/controller/EmployeeController.java
@@ -3,23 +3,25 @@ package com.hackerthon.controller;
 import com.hackerthon.bo.BOFactory;
 import com.hackerthon.bo.custom.EmployeeBO;
 
-public class EmployeeController {
+public class EmployeeController { //create Employee Controller class
 
+	//create BOFactory and get instance with BOType and cast to EmployeeBO
 	static EmployeeBO bo = (EmployeeBO) BOFactory.getInstance().getBOType(BOFactory.BOType.EMPLOYEE);
 
-	public void createEmployeeTable() throws Exception {
-		bo.createEmployeeTable();
+	public void createEmployeeTable()
+			throws Exception {//create employee table and throw exception if pass any exception
+		bo.createEmployeeTable();//call createEmployeeTable method in EmployeeBO
 	}
 
-	public void insertEmployee() throws Exception {
-		bo.addEmployee();
+	public void insertEmployee() throws Exception {//insert employee and throw exception if pass any exception
+		bo.addEmployee();//call addEmployee method in EmployeeBO
 	}
 
-	public void loadAllEmployees() throws Exception {
-		bo.loadAllEmployees();
+	public void loadAllEmployees() throws Exception {//load all employees and throw exception if pass any exception
+		bo.loadAllEmployees();//call loadAllEmployees method in EmployeeBO
 	}
 
-	public void getAllEmployees() throws Exception {
-		bo.getAllEmployee();
+	public void getAllEmployees() throws Exception {//get all employees and throw exception if pass any exception
+		bo.getAllEmployee();//call getAllEmployee method in EmployeeBO
 	}
 }
