3c84cbac4a1ed7a62a6e1966a118ff9f49b9e411
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-10-24 09:31:45 +0530

    add comments to the main class

--- src/com/hackerthon/main/Main.java
+++ src/com/hackerthon/main/Main.java
@@ -5,14 +5,20 @@ import com.hackerthon.utils.FileConverter;
 
 public class Main {
 	public static void main(String[] args) {
+		//create object from EmployeeController
 		EmployeeController employeeController = new EmployeeController();
 		try {
+			// Read the file and request to transform
 			FileConverter.getRequestTransform();
+			// Get the xpaths from the transformed file
 			employeeController.loadAllEmployees();
+			// Create the table
 			employeeController.createEmployeeTable();
+			// Insert the data to the table
 			employeeController.insertEmployee();
+			// Get all the data from the table
 			employeeController.getAllEmployees();
-			//			employeeController.insertEmployee();
+
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
